1cc7ebacbc641d36711f1d203d648e9a3f18b142
BTunney92 <G00270683@gmit.ie>
2022-11-06 12:41:51 +0000

    Assignment 4

--- config.py
+++
@@ -1,3 +0,0 @@
-config = {
-    "apikey": "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"
-}
--- import json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('books.json', 'r+') as file:
-    content = file.read()
-    file.seek(0)
-    content.replace('andrew', 'brendan')
-    file.write(content)
-    
-    
